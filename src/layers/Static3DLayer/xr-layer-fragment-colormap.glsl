983008390b0e9b63855def6f8baf882b6445098a
ilan-gold <ilanbassgold@gmail.com>
2020-10-23 21:30:23 -0400

    Fix frag colormap shader

--- src/layers/Static3DLayer/xr-layer-fragment-colormap.glsl
+++ src/layers/Static3DLayer/xr-layer-fragment-colormap.glsl
@@ -57,13 +57,17 @@ uniform highp usampler3D volume4;
 uniform highp usampler3D volume5;
 
 uniform vec3 dimensions;
-uniform float dt_scale;
 
 // range
 uniform vec2 sliderValues[6];
 
 // color
 uniform vec3 colorValues[6];
+
+// slices
+uniform vec2 xSlice;
+uniform vec2 ySlice;
+uniform vec2 zSlice;
 
 in vec3 vray_dir;
 flat in vec3 transformed_eye;
@@ -141,17 +145,22 @@ void main(void) {
   }
   t_hit.x = max(t_hit.x, 0.0);
   vec3 dt_vec = 1.0 / (vec3(dimensions) * abs(ray_dir));
-  float dt = dt_scale * min(dt_vec.x, min(dt_vec.y, dt_vec.z));
+	float extraDt = 1.1;
+  float dt = extraDt * min(dt_vec.x, min(dt_vec.y, dt_vec.z));
 	float offset = wang_hash(int(gl_FragCoord.x + 640.0 * gl_FragCoord.y));
 	vec3 p = transformed_eye + (t_hit.x + offset * dt) * ray_dir;
 	// TODO: Probably want to stop this process at some point to improve performance when marching down the edges.
 	for (float t = t_hit.x; t < t_hit.y; t += dt) {
-    float intensityValue0 = max((float(texture(volume0, p).r) - sliderValues[0][0]) / sliderValues[0][1], 0.0);
-    float intensityValue1 = max((float(texture(volume1, p).r) - sliderValues[1][0]) / sliderValues[1][1], 0.0);
-		float intensityValue2 = max((float(texture(volume2, p).r) - sliderValues[2][0]) / sliderValues[2][1], 0.0);
-		float intensityValue3 = max((float(texture(volume3, p).r) - sliderValues[3][0]) / sliderValues[3][1], 0.0);
-    float intensityValue4 = max((float(texture(volume4, p).r) - sliderValues[4][0]) / sliderValues[4][1], 0.0);
-		float intensityValue5 = max((float(texture(volume5, p).r) - sliderValues[5][0]) / sliderValues[5][1], 0.0);
+		float canShowXCoordinate = max(p.x - xSlice[0], 0.0) * max(xSlice[1] - p.x , 0.0);
+		float canShowYCoordinate = max(p.y - ySlice[0], 0.0) * max(ySlice[1] - p.y , 0.0);
+		float canShowZCoordinate = max(p.z - zSlice[0], 0.0) * max(zSlice[1] - p.z , 0.0);
+		float canShowCoordinate = float(ceil(canShowXCoordinate * canShowYCoordinate * canShowZCoordinate));
+    float intensityValue0 = canShowCoordinate * max((float(texture(volume0, p).r) - sliderValues[0][0]) / sliderValues[0][1], 0.0);
+    float intensityValue1 = canShowCoordinate * max((float(texture(volume1, p).r) - sliderValues[1][0]) / sliderValues[1][1], 0.0);
+		float intensityValue2 = canShowCoordinate * max((float(texture(volume2, p).r) - sliderValues[2][0]) / sliderValues[2][1], 0.0);
+		float intensityValue3 = canShowCoordinate * max((float(texture(volume3, p).r) - sliderValues[3][0]) / sliderValues[3][1], 0.0);
+    float intensityValue4 = canShowCoordinate * max((float(texture(volume4, p).r) - sliderValues[4][0]) / sliderValues[4][1], 0.0);
+		float intensityValue5 = canShowCoordinate * max((float(texture(volume5, p).r) - sliderValues[5][0]) / sliderValues[5][1], 0.0);
 
 		float intensityArray[6] = float[6](intensityValue0, intensityValue1, intensityValue2, intensityValue3, intensityValue4, intensityValue5);
 		float total = 0.0;
@@ -165,7 +174,7 @@ void main(void) {
 		vec4 val_color = vec4(colormapFunction(total).xyz, total);
 
 		// Opacity correction
-		val_color.a = 1.0 - pow(1.0 - val_color.a, dt_scale);
+		val_color.a = 1.0 - pow(1.0 - val_color.a, 1.0);
 		color.rgb += (1.0 - color.a) * val_color.a * val_color.rgb;
 		color.a += (1.0 - color.a) * val_color.a;
 		if (color.a >= 0.95) {
