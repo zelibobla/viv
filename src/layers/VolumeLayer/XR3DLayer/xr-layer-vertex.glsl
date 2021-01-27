de55733a922c97489aae584e8e7476ecaee4ada9
ilan-gold <ilanbassgold@gmail.com>
2021-01-27 15:51:05 -0500

    Add comment.

--- src/layers/VolumeLayer/XR3DLayer/xr-layer-vertex.glsl
+++ src/layers/VolumeLayer/XR3DLayer/xr-layer-vertex.glsl
@@ -11,6 +11,7 @@ void main() {
   // This puts the center at the midpoint of the dimensions.
   vec3 coords = project_position(positions * dimensions);
   gl_Position = project_common_position_to_clipspace(vec4(coords, 1.0));
+  // Camera position from deck.gl is has the origin subtracted off from it.
   transformed_eye = (project_uCameraPosition + project_uCoordinateOrigin) / dimensions;
 	vray_dir = positions - transformed_eye;
 }
